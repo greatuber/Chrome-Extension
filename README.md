fcc78786085e0aa413b66b3ef96cbaa8f38911f8
sabugaa0128 <sabugaarozvelt@gmail.com>
2022-05-24 15:11:56 +0900

    re-branding

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# chrome-extension
-personal chrome extension
+# Chrome Extension
+
+My personal Chrome extensions.
+
+# Why
+
+- Installing an unknown Chrome extension is sometimes harmful as it might steal data.
+- It is possible to inject any kind of code snnipets into any websites because it is unpacked extension.
+- Creating a Chrome extension is easier and more fun to search extensions on the Chrome store and try it.
---
+++ sites/meet.google.com/index.js
@@ -0,0 +1,19 @@
+/**
+ * Maximize presentation with M-z.
+ */
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'z' && event.altKey) {
+    document.querySelector('[data-allocation-index="0"]').requestFullscreen()
+  }
+})
+
+// /**
+//  * Autojoin meeting.
+//  */
+// MutationObserver.observe(document.body, (mutations) => {
+//   mutations.forEach((mutation) => {
+//     if (mutation.target.classList.contains('is-loading')) {
+//       document.querySelector('[data-allocation-index="0"]').click()
+//     }
+//   })
+// })
