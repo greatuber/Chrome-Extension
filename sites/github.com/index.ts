80bb0e30a9afb6cf5f08f489d9d61f8f605f64e3
sabugaa0128 <sabugaarozvelt@gmail.com>
2023-01-31 23:52:33 +0900

    github fixed the bug, so commented out my plugin

--- sites/github.com/index.ts
+++ sites/github.com/index.ts
@@ -1,48 +1,48 @@
-import { isInputting } from '../../util/dom'
-
-/**
- * Move rows with j/k
- */
-let fileTreeIndex = 0
-let currentUrl = window.location.href
-
-function draw(givenRows?: Element[]) {
-  const rows =
-    givenRows || document.querySelectorAll('.js-navigation-item[role="row"]')
-  for (let i = 0; i < rows.length; i++) {
-    const row = rows[i] as HTMLDivElement
-    if (i === fileTreeIndex) {
-      row.style.backgroundColor = '#1e293b'
-      row.querySelector('a')?.focus()
-    } else {
-      row.style.backgroundColor = 'unset'
-    }
-  }
-}
-
-document.addEventListener('keydown', (event) => {
-  if (isInputting) {
-    return
-  }
-  if (event.key === 'j') {
-    const rows = document.querySelectorAll('.js-navigation-item[role="row"]')
-    if (fileTreeIndex < rows.length - 1) {
-      fileTreeIndex++
-    }
-    draw(rows as any)
-  }
-  if (event.key === 'k') {
-    if (0 < fileTreeIndex) {
-      fileTreeIndex--
-    }
-    draw()
-  }
-})
-
-setInterval(() => {
-  if (currentUrl !== window.location.href) {
-    fileTreeIndex = 0
-    draw()
-    currentUrl = window.location.href
-  }
-}, 500)
+// import { isInputting } from '../../util/dom'
+//
+// /**
+//  * Move rows with j/k
+//  */
+// let fileTreeIndex = 0
+// let currentUrl = window.location.href
+//
+// function draw(givenRows?: Element[]) {
+//   const rows =
+//     givenRows || document.querySelectorAll('.js-navigation-item[role="row"]')
+//   for (let i = 0; i < rows.length; i++) {
+//     const row = rows[i] as HTMLDivElement
+//     if (i === fileTreeIndex) {
+//       row.style.backgroundColor = '#1e293b'
+//       row.querySelector('a')?.focus()
+//     } else {
+//       row.style.backgroundColor = 'unset'
+//     }
+//   }
+// }
+//
+// document.addEventListener('keydown', (event) => {
+//   if (isInputting) {
+//     return
+//   }
+//   if (event.key === 'j') {
+//     const rows = document.querySelectorAll('.js-navigation-item[role="row"]')
+//     if (fileTreeIndex < rows.length - 1) {
+//       fileTreeIndex++
+//     }
+//     draw(rows as any)
+//   }
+//   if (event.key === 'k') {
+//     if (0 < fileTreeIndex) {
+//       fileTreeIndex--
+//     }
+//     draw()
+//   }
+// })
+//
+// setInterval(() => {
+//   if (currentUrl !== window.location.href) {
+//     fileTreeIndex = 0
+//     draw()
+//     currentUrl = window.location.href
+//   }
+// }, 500)
