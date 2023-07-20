27d54ca6c0ce21733b0359927f8300a22da37f27
sabugaa0128 <sabugaarozvelt@gmail.com>
2023-07-20 12:38:54 +0900

    fix: do not misclick

--- sites/chat.openai.com/index.ts
+++ sites/chat.openai.com/index.ts
@@ -2,11 +2,11 @@
  * Maximize presentation with M-z.
  */
 document.addEventListener('keyup', (event) => {
-  console.log(event)
   if (event.key === 'o' && event.altKey) {
     for (const elm of document.querySelectorAll('a')) {
       if (elm.textContent?.includes('New chat')) {
         elm.click()
+        break
       }
     }
   }
